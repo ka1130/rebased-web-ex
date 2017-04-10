07cbe19174b44b9802d542077016cec123247886
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-04-10 13:48:18 +0200

    Add opening for mobile menu.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 (function () {
   document.addEventListener("DOMContentLoaded", function (event) {
 
+    const hamburger = document.getElementsByTagName("nav")[0];
+    const menuList = hamburger.firstElementChild;
+
+    function openNav(event) {
+      menuList.style.height = "calc(100vh - 7.9rem)";
+    }
+
+    hamburger.addEventListener("click", openNav, false);
 
   });
 })();

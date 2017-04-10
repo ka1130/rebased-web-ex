bda1983fa0c6ae95756cb8964b37f73fd5e3ff8f
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-04-10 14:19:43 +0200

    Mobile menu working.

--- js/app.js
+++ js/app.js
@@ -3,12 +3,24 @@
 
     const hamburger = document.getElementsByTagName("nav")[0];
     const menuList = hamburger.firstElementChild;
+    const listItems = menuList.getElementsByTagName("a");
 
-    function openNav(event) {
-      menuList.style.height = "calc(100vh - 7.9rem)";
+    function openCloseNav(event) {
+      if (menuList.style.height == "0px") {
+        menuList.style.height = "calc(100vh - 7.9rem)";
+        for (let i = 0; i < listItems.length; i++) {
+          listItems[i].style.visibility = "visible";
+        }
+
+      } else {
+        menuList.style.height = "0px";
+        for (let i = 0; i < listItems.length; i++) {
+          listItems[i].style.visibility = "hidden";
+        }
+      }
     }
 
-    hamburger.addEventListener("click", openNav, false);
+    hamburger.addEventListener("click", openCloseNav, false);
 
   });
 })();

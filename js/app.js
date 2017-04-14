43efb4dca06b71f489594dd33e42f5689d93faef
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-04-14 11:54:03 +0200

    Change function for mobile menu.

--- js/app.js
+++ js/app.js
@@ -7,27 +7,18 @@
     const menuList = nav.firstElementChild;
     const listItems = menuList.getElementsByTagName("a");
 
+    // menuList.classList.add("menu-inactive");
+
+    nav.addEventListener("click", function (event) {
+      menuList.classList.toggle("menu-active");
+    }, false);
+
+
+    // Change color of the hamburger on click
+
     let hamburgerStyle = window.getComputedStyle(nav, ":before");
 
     console.dir(hamburgerStyle);
-
-    function openCloseNav(event) {
-
-      if (menuList.style.height == "0px") {
-        menuList.style.height = "calc(100vh - 5rem)";
-        for (let i = 0; i < listItems.length; i++) {
-          listItems[i].style.visibility = "visible";
-        }
-
-      } else {
-        menuList.style.height = "0px";
-        for (let i = 0; i < listItems.length; i++) {
-          listItems[i].style.visibility = "hidden";
-        }
-      }
-    }
-
-    nav.addEventListener("click", openCloseNav, false);
 
 
     // 

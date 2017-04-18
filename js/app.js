d346a650747684235ad440e8c912a2138991eb36
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-04-18 10:13:23 +0200

    Remove unnecessary fonts.

--- js/app.js
+++ js/app.js
@@ -15,17 +15,17 @@
 
     // Team memebers back
 
-    const membersList = Array.from(document.querySelector("main").children[1].children[1].children);
-    console.log(membersList);
+    // const membersList = Array.from(document.querySelector("main").children[1].children[1].children);
+    // console.log(membersList);
 
-    membersList.find(member => {
-      let figures = member.children[0];
-      console.log(figures);
-      member.addEventListener("click", function (event) {
-        figures.classList.toggle("member-back");
-      }, false)
+    // membersList.find(member => {
+    //   let figures = member.children[0];
+    //   console.log(figures);
+    //   member.addEventListener("click", function (event) {
+    //     figures.classList.toggle("member-back");
+    //   }, false)
 
-    });
+    // });
 
 
 

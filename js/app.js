f75f3f7ea559d71a60600670b9f5b018c640595f
Kamila Matla-Tomczyk <ka1130@users.noreply.github.com>
2017-04-24 15:40:04 +0200

    Initial pull request. (#2)
    * Optimized declarations.
    
    * Remove unnecessary fonts.
    
    * Add sections' ids.
    
    * Change units.
    
    * Change pixels to rems.
    
    * Change figures' structure.
    
    * Fighting with CSS.
    
    * Hover structure for team section.
    
    * Fiished hover effect in the team section.
    
    * Rewrite figcaptions to flex.
    
    * Fix images in the Team section.
    
    * Fix community section.
    
    * Fixed community section.
    
    * Fix contact section.
    
    * Finished Contact section.
    
    * Styled Contact section for tablets.
    
    * Ordered media queries.
    
    * Media queries for tablets.
    
    * Solve open-source grid and vw units issue.
    
    * Home section on desktops.
    
    * Change @font-face to Google fonts link.
    
    * Fix moble view.
    
    * Corrected code as per Frey's review.
    
    * Add divider.
    
    * Fix mobile layout.
    
    * Start working on Projects' section on desktops.
    
    * Add structure to comments.
    
    * Fix header on desktops.
    
    * Finish main section on desktops.
    
    * Fix menu.
    
    * Fix team section.
    
    * Fix projects section on desktops.
    
    * Finish open-source section on desktops.
    
    * Finish community section for desktops.
    
    * Fix footer on desktops.
    
    * Fix background.
    
    * Minor corrections in the team section.
    
    * Hide unnecessary images.
    
    * Function showImages working.
    
    * Fix function in the team section.
    
    * Button rotate on click.
    
    * Change photos of tema members.
    
    * Correct home-claim section.
    
    * Fix background foooter's sizing.
    
    * Fix desktop view.
    
    * Block scroll on body when menu is open.
    
    * Move all width declarations to one place.
    
    * Add scrolling function.
    
    * Fix menu behaviour on desktops.
    
    * Only two team members visible on mobiles.
    
    * Fix styles as per code review.
    
    * Clean up .css
    
    * Correct styles for fieldset.
    
    * New version - checking the fonts.
    
    * Remove unnecessary font.
    
    * Correct distances in the projects section / desktops.
    
    * Change main's footer color on XL screens.
    
    * Fix address positioning on desktops.
    
    * Correct styles.
    
    * Correct typo.

--- js/app.js
+++ js/app.js
@@ -6,33 +6,80 @@
     const nav = document.getElementsByTagName("nav")[0];
     const menuList = nav.firstElementChild;
     const listItems = menuList.getElementsByTagName("a");
+    const body = document.getElementsByTagName("body")[0];
+    const mq = window.matchMedia("(max-width: 768px)");
 
     nav.addEventListener("click", function (event) {
-      menuList.classList.toggle("menu-active");
-      nav.classList.toggle("hamburger-active");
+      if (mq.matches) {
+        menuList.classList.toggle("menu-active");
+        nav.classList.toggle("hamburger-active");
+        body.classList.toggle("noScroll");
+      }
     }, false);
 
+    // Team load more
 
-    // Team memebers back
+    const arrowBtn = document.getElementById("team").children[1].nextElementSibling.nextElementSibling.children[0];
+    const teamImages = document.getElementById("team").children[1].children;
 
-    const membersList = Array.from(document.querySelector("main").children[1].children[1].children);
-    console.log(membersList);
+    if (mq.matches) {
+      for (var i = 0; i < teamImages.length; i++) {
+        if (i > 1) {
+          teamImages[i].style.display = "none";
+        }
+      }
+    } else {
+      for (var i = 0; i < teamImages.length; i++) {
+        if (i > 5) {
+          teamImages[i].style.display = "none";
+        }
+      }
+    }
 
-    membersList.find(member => {
-      let figures = member.children[0];
-      console.log(figures);
-      member.addEventListener("click", function (event) {
-        figures.classList.toggle("member-back");
-      }, false)
+    function loadMore(event) {
+      event.preventDefault();
+      if (mq.matches) {
+        for (let i = 0; i < teamImages.length; i++) {
+          if (i > 1) {
+            if (teamImages[i].style.display === "none") {
+              teamImages[i].style.display = "list-item";
+              event.target.classList.add("btnRotated");
+            } else {
+              teamImages[i].style.display = "none";
+              event.target.classList.remove("btnRotated");
+            }
+          }
+        }
+      } else {
+        for (let i = 0; i < teamImages.length; i++) {
+          if (i > 5) {
+            if (teamImages[i].style.display === "none") {
+              teamImages[i].style.display = "list-item";
+              event.target.classList.add("btnRotated");
+            } else {
+              teamImages[i].style.display = "none";
+              event.target.classList.remove("btnRotated");
+            }
+          }
+        }
+      }
+    }
 
+    arrowBtn.addEventListener("click", loadMore, false);
+
+    //Scroll animation
+
+    const menuLinks = $("nav li").not(":first-child, :nth-child(3)").children();
+
+    menuLinks.on("click", function (event) {
+      event.preventDefault();
+      let myId = $(event.target).attr("href");
+      $("html, body").animate({
+        scrollTop: $(myId).offset().top - 100
+      }, 500);
     });
 
-
-
-
-
-
-
+    //end
 
   });
 })();

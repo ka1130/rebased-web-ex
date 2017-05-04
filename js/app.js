5e020ffa22e8c66b5c5f38878e7b29539462c874
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-05-04 13:19:51 +0200

    Rewrite JS

--- js/app.js
+++ js/app.js
@@ -12,11 +12,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   document.getElementById("show-more").addEventListener("click", function (event) {
     event.preventDefault();
-    if (window.matchMedia("(max-width: 768px)").matches) {
-      document.getElementById("team").classList.toggle("team-active");
-    } else {
-      document.getElementById("team").classList.toggle("team-active");
-    }
+    document.getElementById("team").classList.toggle("team-active");
   }, false);
 
   //end

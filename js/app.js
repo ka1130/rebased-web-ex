2e144ba19ec5aa4db1407aa697d4f83be594f489
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-05-04 13:09:49 +0200

    Rewrite JS functions

--- js/app.js
+++ js/app.js
@@ -1,85 +1,51 @@
-(function () {
-  document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
-    // Hamburger Menu
+  // Hamburger Menu
 
-    const nav = document.getElementsByTagName("nav")[0];
-    const menuList = nav.firstElementChild;
-    const listItems = menuList.getElementsByTagName("a");
-    const body = document.getElementsByTagName("body")[0];
-    const mq = window.matchMedia("(max-width: 768px)");
+  document.querySelector("#main-nav").addEventListener("click", function (event) {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      document.querySelector("body").classList.toggle("menu-active");
+    }
+  }, false);
 
-    nav.addEventListener("click", function (event) {
-      if (mq.matches) {
-        menuList.classList.toggle("menu-active");
-        nav.classList.toggle("hamburger-active");
-        body.classList.toggle("noScroll");
+  // Team load more
+
+  const arrowBtn = document.getElementById("team").children[1].nextElementSibling.nextElementSibling.children[0];
+  const teamImages = document.getElementById("team").children[1].children;
+
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    for (var i = 0; i < teamImages.length; i++) {
+      if (i > 1) {
+        teamImages[i].style.display = "none";
       }
-    }, false);
-
-    // Team load more
-
-    const arrowBtn = document.getElementById("team").children[1].nextElementSibling.nextElementSibling.children[0];
-    const teamImages = document.getElementById("team").children[1].children;
-
-    if (mq.matches) {
-      for (var i = 0; i < teamImages.length; i++) {
-        if (i > 1) {
-          teamImages[i].style.display = "none";
-        }
+    }
+  } else {
+    for (var i = 0; i < teamImages.length; i++) {
+      if (i > 5) {
+        teamImages[i].style.display = "none";
       }
+    }
+  }
+
+  document.getElementById("show-more").addEventListener("click", function (event) {
+    event.preventDefault();
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      document.getElementById("team").classList.toggle("team-active");
     } else {
-      for (var i = 0; i < teamImages.length; i++) {
+      for (let i = 0; i < teamImages.length; i++) {
         if (i > 5) {
-          teamImages[i].style.display = "none";
-        }
-      }
-    }
-
-    function loadMore(event) {
-      event.preventDefault();
-      if (mq.matches) {
-        for (let i = 0; i < teamImages.length; i++) {
-          if (i > 1) {
-            if (teamImages[i].style.display === "none") {
-              teamImages[i].style.display = "list-item";
-              event.target.classList.add("btnRotated");
-            } else {
-              teamImages[i].style.display = "none";
-              event.target.classList.remove("btnRotated");
-            }
-          }
-        }
-      } else {
-        for (let i = 0; i < teamImages.length; i++) {
-          if (i > 5) {
-            if (teamImages[i].style.display === "none") {
-              teamImages[i].style.display = "list-item";
-              event.target.classList.add("btnRotated");
-            } else {
-              teamImages[i].style.display = "none";
-              event.target.classList.remove("btnRotated");
-            }
+          if (teamImages[i].style.display === "none") {
+            teamImages[i].style.display = "list-item";
+            event.target.classList.add("btnRotated");
+          } else {
+            teamImages[i].style.display = "none";
+            event.target.classList.remove("btnRotated");
           }
         }
       }
     }
+  }, false);
 
-    arrowBtn.addEventListener("click", loadMore, false);
+  //end
 
-    //Scroll animation
-
-    const menuLinks = $("nav li").not(":first-child, :nth-child(3)").children();
-
-    menuLinks.on("click", function (event) {
-      event.preventDefault();
-      let myId = $(event.target).attr("href");
-      $("html, body").animate({
-        scrollTop: $(myId).offset().top - 100
-      }, 500);
-    });
-
-    //end
-
-  });
-})();
+});

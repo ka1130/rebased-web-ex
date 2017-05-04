94fbc6d8c1a52354be651139b5d662f303041ffb
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-05-04 13:12:52 +0200

    Rewrite function showing team members

--- js/app.js
+++ js/app.js
@@ -10,39 +10,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   // Team load more
 
-  const arrowBtn = document.getElementById("team").children[1].nextElementSibling.nextElementSibling.children[0];
-  const teamImages = document.getElementById("team").children[1].children;
-
-  if (window.matchMedia("(max-width: 768px)").matches) {
-    for (var i = 0; i < teamImages.length; i++) {
-      if (i > 1) {
-        teamImages[i].style.display = "none";
-      }
-    }
-  } else {
-    for (var i = 0; i < teamImages.length; i++) {
-      if (i > 5) {
-        teamImages[i].style.display = "none";
-      }
-    }
-  }
-
   document.getElementById("show-more").addEventListener("click", function (event) {
     event.preventDefault();
     if (window.matchMedia("(max-width: 768px)").matches) {
       document.getElementById("team").classList.toggle("team-active");
     } else {
-      for (let i = 0; i < teamImages.length; i++) {
-        if (i > 5) {
-          if (teamImages[i].style.display === "none") {
-            teamImages[i].style.display = "list-item";
-            event.target.classList.add("btnRotated");
-          } else {
-            teamImages[i].style.display = "none";
-            event.target.classList.remove("btnRotated");
-          }
-        }
-      }
+      document.getElementById("team").classList.toggle("team-active");
     }
   }, false);
 
